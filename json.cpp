be3c07352e896b800d6a1ab88de038f4087f6586
Artyom Beilis <artyomtnk@yahoo.com>
2009-06-05 18:59:06 +0000

    Some updates... line up

--- json.cpp
+++ json.cpp
@@ -449,159 +449,332 @@ namespace json {
 	{
 		return !(*this == other);
 	}
+/*
+	namespace {
+
+	enum {	tock_eof = 256,
+		tock_err,
+		tock_str,
+		tock_bool,
+		tock_null,
+		tock_num }
+
+	std::pair<int,value> tocken(int c)
+	{
+		return std::pair<int,value>(c,value());
+	}
+	std::pair<int,value> tocken(int c,value const &v)
+	{
+		return std::pair<int,value>(c,v);
+	}
+
+
+	std::pair<int,value> check_string(std::string::const_iterator &p,std::string::const_iterator e)
+	{
+		std::string result;
+		bool prev_surragate=false;
+		uint16_t upair[2];
+		for(;p!=e;) {
+			std::string::const_iterator prev=p;
+			uint32_t c=utf8::next(p,e,false,true); // Decode, ignore special
+			if(prev_surragate){
+				if(!(c=='\\' && p+1!=e && *(p+1)=='u'))
+					return tocken(tock_err);
+			}
+			if(c==utf::illegal)
+				return tocken(tock_err);
+			else if(c=='"'){
+				value v=result;
+				return tocken(tock_str,v);
+			}
+			else if(c=='\\') {
+				if(p==e) return tocken(tock_err);
+				c=(unsigned char)*p++;
+				switch(c) {
+				case '\\': result+='\\'; break;
+				case '/': result+='/'; break;
+				case 'n': result+='\n'; break;
+				case 'r': result+='\r'; break;
+				case 't': result+='\t'; break;
+				case 'f': result+='\f'; break;
+				case 'b': result+='\b'; break;
+				case 'u':
+					{
+						uint16_t x=0;
+						for(int n=0;n<4;n++) {
+							if(p!=e) return tocken(tock_err);
+							c=*p++;
+							if('0'<=c && c<='9')
+								x=(x<<4) | (c-'0')
+							else if('a'<=c && c<='f')
+								x=(x<<4) | (c-'a'+10)
+							else if('A'<=c && c<='F')
+								x=(x<<4) | (c-'A'+10)
+							else
+								return tocken(tock_err);
+						}
+						if(prev_surragate && 0xDC00 <=x && x<=0xDFFF) {
+							upair[1]=x;
+							uint16_t *ptr=upair;
+							prev_surragate=false;
+							result+=utf8::encode(utf16::next(ptr,ptr+2)).c;
+						}
+						else if(prev_surragate)
+							return tocken(tock_err);
+						else if(0xD800 <= x && x<=0xDBFF) {
+							prev_surragate=true;
+							upair[0]=x;
+						}
+						else if(0xDC00 <=x && x<=0xDFFF)
+							return tocken(tock_err);
+						else if(x==0)
+							result+='\0';
+						else 
+							result+=utf8::encode(x).c;
+						break;
+
+					}
+				case '"': result+='"'; break;
+				default: return tocken(tock_err);
+				}
+			}
+			else {
+				result.append(prev,p);
+			}
+		}
+	}
+	std::pair<int,value> check_number(std::string::const_iterator &p,std::string::const_iterator e)
+	{
+		enum {	start,
+			minus_found,
+			diggit_found,
+			point_found,
+			exp_found,
+			exp_sign_found,
+			exp_digit_found
+			stop,
+			syntax}
+		state=start;
+		std::string::const_iterator begin=p;
+		
+		for(;state!=stop && state!=syntax && p!=e;) {
+			c=*p++;
+			switch(state) {
+			case start:
+				if(c=='-')
+					stat=minus_found;
+				else if('1'<=c && c<='9')
+					state=diggit_found;
+				else if(c=='0')
+					state=stop;
+				else
+					state=syntax;
+				break;
+			case minus_found:
+				if('1'<=c && c<='9')
+					state=digit_found;
+				else if(c=='.')
+					state=point_found;
+				else
+					state=syntax;
+				break;
+			case digit_found:
+				if('0'<=c && c<='9')
+					;
+				else if(c=='.')
+					state=point_found;
+				else if(c=='e' || c=='E')
+					state=exp_found;
+				else
+					state=stop;
+				break;
+			case point_found:
+				if('0'<=c && c<='9')
+					;
+				else if(c=='e' || c=='E')
+					state=exp_found;
+				else
+					state=stop;
+				break;
+			case exp_found:
+				if(c=='-' || c=='+')
+					state=exp_sign_found;
+				else if('1'<=c && c<='9')
+					state=exp_digit_found;
+				else
+					state=syntax;
+				break;
+			case exp_sign_found:
+				if('1'<=c && c<='9')
+					state=exp_digit_found;
+				else
+					state=syntax;
+				break;
+			case exp_digit_found:
+				if('0'<=c && c<='9')
+					;
+				else
+					state=stop;
+			default:
+				throw std::runtime_error("Internal state error");:			
+			}
+		}
+		if(state==syntax || state==minus_found || state==exp_found || state==exp_sign_found)
+			return tocken(tock_err);
+		if(p!=e)
+			p--;
+		std::string num(begin,p);
+		value v=atof(num.c_str());
+		return tocken(tock_num,v);
+	}
+
+
+	std::pair<int,value> next_tocken(std::string::const_iterator &p,std::string::const_iterator e,int &lines)
+	{
+		if(p==e) {
+			return tocken(tock_eof);
+		}
+		while(p!=e) {
+			char c=*p++;
+			switch(c) {
+			case	'/':
+				if(p!=e && *p=='/') {
+					while(p!=e && '\n'!=*p++)
+						;
+					break;
+				}
+				return tocken(tock_err);
+
+			case	'\r':
+				lines++;
+				// Fall
+			case	' ':
+			case	'\t':
+			case	'\n':	
+			case	'\f':
+
+				break;
+
+			case	'[':
+			case	'{':
+			case	']':
+			case	'}':
+			case	',':
+			case	':':
+
+				return tocken(c);
+			case	't':
+				if(	p!=e && 'r'==*p++
+					&& p!=e && 'u'==*p++
+					&& p!=e && 'e'==*p++)
+				{
+					value v=true;
+					return tocken(tock_bool,v);
+				}
+				return tocken(tock_err);
+			case	'f':
+				if(	p!=e && 'a'==*p++
+					&& p!=e && 'l'==*p++
+					&& p!=e && 's'==*p++
+					&& p!=e && 'e'==*p++)
+				{
+					value v=false;
+					return tocken(tock_bool,v);
+				}
+				return tocken(error);
+			case	'n':
+				if(	p!=e && 'u'==*p++
+					&& p!=e && 'l'==*p++
+					&& p!=e && 'l'==*p++)
+				{
+					return tocken(tock_null);
+				}
+				return tocken(tock_err);
+			case	'"':
+				return check_string(p,e);
+			case	'-':
+			case	'0':
+			case	'1':
+			case	'2':
+			case	'3':
+			case	'4':
+			case	'5':
+			case	'6':
+			case	'7':
+			case	'8':
+			case	'9':
+				p--;
+				return check_number(p,e);
+			default:
+				return tocken(tock_err);
+			}
+		}
+		return tocken(tock_eof);
+	}
+
+	} // anon namespace
+	int value::load(std::string const &s)
+	{
+		lines=0;
+		std::string::const_iterator p=s.begin(),e=s.end();
+		stack<value> st;
+		enum {
+			idle,
+			arr_open_found,
+			obj_open_found,
+			arr_comma_expected,
+			obj_col_expected,
+			obj_value_expected,
+			obj_comma_expected,
+			end_of_obj_found,
+			error
+		};
+
+		}
+		int state=idle;
+		value curr;
+		for(;;) {
+			std::pair<int,value> tock=next_tocken(p,e);
+			int tid;
+			switch(state) {
+			case idle:
+				switch(tid) {
+				case '[': state=arr_open_found; curr=array(); break;
+				case '{': state=obj_open_found; curr=object(); break;
+				default: state=error; break;
+				}
+				break;
+			case arr_open_found:
+				switch(tid) {
+				case tock_num:
+				case tock_str:
+				case tock_bool:
+				case tock_null:
+					curr.array().push_back(tock.second);
+					state=arr_comma_expected;
+					break;
+				default: state=error; break;
+				}
+				break;
+			case arr_comma_expected:
+				if(tid==',') { state=arr_open_found; break; }
+				// TODO
+			case obj_open_found:
+				if(tid!=tock_str) { state=error; break; }
+				str=tock.second.str();
+				state=obj_comma_expected;
+				break;
+			case 
+			}
+		}
+	}
+
+*/
 
 	int value::load(std::string const &s)
 	{
 		throw std::runtime_error("Unsupported");
 	}
-
-/*	namespace {
-		struct tockenizer {
-			value::input_stream &input_;
-			tockenizer(value::input_stream  &input) : input_(input)
-			{
-			}
-			std::string string_;
-			double number_;
-			bool boolean_;
-			enum {
-				error = 0,
-				null = 256,
-				string = 257,
-				boolean = 258 ,
-				number = 259,
-				eof = 260
-			};
-			bool read_number()
-			{
-				std::string num;
-				num.reserve(32);
-				c=input_.get();
-				num+=c;
-				if(c=='0') {
-					number_=0;
-					return true;
-				}
-				while((c=input_.get())!=eof && '0'<=c && c<='9')
-					num+=c;
-				if(c=='.') {
-					num+=c;
-					c=input_.get();
-					if(c<'0' || '9'<c)
-						return false;
-					num+=c;
-					while((c=input_.get())!=eof && '0'<=c && c<='9')
-						num+=c;
-				}
-				if(c=='E' || c=='e')
-
-			}
-			int next()
-			{
-				int c=input.get();
-				for(;;) {
-					switch(c) {
-					case -1:
-						return eof;
-					case '\t':
-					case '\f':
-					case '\r':
-					case '\n':
-					case ' ':
-						break;
-					case '{':
-					case '[':
-					case ']':
-					case '}':
-					case ':':
-					case ',':
-						return c;
-					case '/':
-						c=input_.get();
-						if(c=='/') {
-							while((c=input_.get())!=-1 && c!='\n')
-								;
-							if(c==-1)
-								return eof;
-							if(c=='\n')
-								break;
-						}
-						else if(c=='*') {
-							while((c=input_.get())!=-1) {
-								if(c=='*') {
-									c=input_.get()
-									if(c=='/')
-										break;
-									if(c==-1)
-										return error;
-								}
-							}
-							break;
-						}
-						else
-							return error;
-						break;
-					case 't':
-						if(!is("rue"))
-							return error;
-						boolean_=true;
-						return boolean;
-					case 'f':
-						if(!is("alse"))
-							return error;
-						boolean_=false;
-						reurn boolean;
-					case 'n':
-						if(!is("ull"))
-							return error;
-						return null;
-					case '"':
-						input_.unget('"');
-						if(!read_string())
-							return error;
-						return string;
-					case '-':
-					case '0':
-					case '1':
-					case '2':
-					case '3':
-					case '4':
-					case '5':
-					case '6':
-					case '7':
-					case '8':
-					case '9':
-						input_.unget(c);
-						if(!read_number())
-							return error;
-						return number;
-					default:
-						return error;
-					}
-				}
-			}
-		}
-	}
-
-
-	value parse(input_stream &input,bool &ok)
-	{
-		value result;
-		tockenizer tock(input);
-		std::stack<int> stack;
-		ok=true;
-		int tocken;
-		for(;;) {
-			tocken=tock.get();
-			if(tocken==tockenizer::error) {
-				ok=false;
-				return value();
-			}
-			if(tocken==tockenizer::eof)
-
-
-	}*/
-
 
 } // json
 
