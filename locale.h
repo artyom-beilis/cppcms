a9d326ad3a8322325faf33e6f198909a7cf053f8
Artyom Beilis <artyomtnk@yahoo.com>
2009-11-10 22:34:11 +0000

    Looks like CMake even works

---
+++ locale.h
@@ -0,0 +1,13 @@
+#ifndef CPPCMS_LOCALE_H
+#define CPPCMS_LOCALE_H
+#include <boost/locale.hpp>
+
+namespace cppcms {
+	namespace locale {
+		using namespace boost::locale;
+		// Get there all of boost locale
+	}
+}
+
+
+#endif
