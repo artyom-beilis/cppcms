03f9f22fafe458f647ca499e2f7b7a5bbf31b71e
Artyom Beilis <artyomtnk@yahoo.com>
2009-11-24 05:46:25 +0000

    Fixed for Sun Studio

--- service.cpp
+++ service.cpp
@@ -19,6 +19,16 @@
 #include <sys/wait.h>
 #endif
 
+#ifndef CPPCMS_WIN32
+#if defined(SOLARIS) || defined(_SOLARIS)
+#define _POSIX_PTHREAD_SEMANTICS
+#endif
+#include <signal.h>
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <iostream>
 #include <fstream>
 #include <boost/lexical_cast.hpp>
