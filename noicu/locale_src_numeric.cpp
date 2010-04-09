2b0f305a7a7a06b0428e5195a4203fe2a6d621d4
Artyom Beilis <artyomtnk@yahoo.com>
2010-04-09 15:30:29 +0000

    Updated license to LGPLv3

--- noicu/locale_src_numeric.cpp
+++ noicu/locale_src_numeric.cpp
@@ -1,3 +1,10 @@
+//
+//  Copyright (c) 2009-2010 Artyom Beilis (Tonkikh)
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
 #define CPPCMS_LOCALE_SOURCE
 #include "locale_numeric.h"
 #include "config.h"
--- template.h
+++ template.h
@@ -16,27 +16,3 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 ///////////////////////////////////////////////////////////////////////////////
-#ifndef CPPCMS_CACHE_POOL_H
-#define CPPCMS_CACHE_POOL_H
-
-#include "defs.h"
-#include "noncopyable.h"
-#include "intrusive_ptr.h"
-#include "hold_ptr.h"
-
-namespace cppcms {
-	namespace json { class value; }
-	namespace impl { class base_cache; }
-	
-	class CPPCMS_API cache_pool {
-	public:
-		cache_pool(json::value const &settings);
-		~cache_pool();
-		intrusive_ptr<impl::base_cache> get();
-	private:
-		struct data;
-		util::hold_ptr<data> d;
-	};
-}
-
-#endif
