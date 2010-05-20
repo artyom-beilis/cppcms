279fc5068cd3d7c67fa7482893323b2b948d15f7
Artyom Beilis <artyomtnk@yahoo.com>
2010-05-20 16:23:11 +0000

    Reversed back to "clone_ptr" - for better semantics This is not a correct point to improve

--- booster/booster/function.h
+++ booster/booster/function.h
@@ -9,8 +9,7 @@
 #define BOOSTER_FUNCTION_H
 
 #include <stdexcept>
-#include <booster/intrusive_ptr.h>
-#include <booster/refcounted.h>
+#include <booster/clone_ptr.h>
 
 namespace booster {
 	template<typename Type>
@@ -24,14 +23,15 @@ namespace booster {
 		}
 	};
 
-	#define BOOSTER_FUNCTION						\
+	#define BOOSTER_FUNCTION							\
 	template<typename Result BOOSTER_TEMPLATE_PARAMS >			\
 	class function<Result(BOOSTER_TYPE_PARAMS)>				\
 	{									\
 	public:									\
 		typedef Result result_type;					\
-		struct callable : public refcounted {				\
+		struct callable {						\
 			virtual Result call(BOOSTER_TYPE_PARAMS) =0;		\
+			virtual callable *clone() const = 0;			\
 			virtual ~callable(){}					\
 		};								\
 										\
@@ -41,6 +41,8 @@ namespace booster {
 			callable_impl(F f) : func(f){}				\
 			virtual R call(BOOSTER_TYPE_PARAMS) 			\
 			{  return func(BOOSTER_CALL_PARAMS); }			\
+			virtual callable *clone() const				\
+			{ return new callable_impl<R,F>(func); }		\
 		};								\
 		template<typename F>						\
 		struct callable_impl<void,F> : public callable {		\
@@ -48,12 +50,20 @@ namespace booster {
 			callable_impl(F f) : func(f){}				\
 			virtual void call(BOOSTER_TYPE_PARAMS) 			\
 			{  func(BOOSTER_CALL_PARAMS); }				\
+			virtual callable *clone() const				\
+			{ return new callable_impl<void,F>(func); }		\
 		};								\
 		function(){}							\
 		template<typename F>						\
 		function(F func) : call_ptr(new callable_impl<Result,F>(func)) 	\
 		{}								\
 		function(function const &other) : call_ptr(other.call_ptr) {}	\
+		template<typename F>						\
+		function const &operator=(F func)				\
+		{ 								\
+			call_ptr.reset(new callable_impl<Result,F>(func));	\
+			return *this;						\
+		}								\
 		function const &operator=(function const &other)		\
 		{ 								\
 			if(this != &other) { call_ptr=other.call_ptr; } 	\
@@ -68,7 +78,7 @@ namespace booster {
 		operator bool() const { return !empty(); }			\
 		void swap(function &other) { call_ptr.swap(other.call_ptr); }	\
 	private:								\
-		intrusive_ptr<callable> call_ptr;				\
+		clone_ptr<callable> call_ptr;					\
 	};									\
 
 	#define BOOSTER_TEMPLATE_PARAMS
@@ -144,5 +154,6 @@ namespace booster {
 	#undef BOOSTER_FUNCTION
 
 } // booster
+
 
 #endif
