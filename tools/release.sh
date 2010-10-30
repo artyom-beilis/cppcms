bf4139b3913439f1de18fd62ed3ec7bd3948e28a
Artyom Beilis <artyomtnk@yahoo.com>
2010-10-30 17:43:34 +0000

    Added release script, removed old changelog

---
+++ tools/release.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+SVNROOT=https://cppcms.svn.sourceforge.net/svnroot/cppcms/framework
+
+svn export $SVNROOT/trunk current-release
+VERSION=`grep CPPCMS_PACKAGE_VERSION current-release/CMakeLists.txt  | sed 's/.*"\(.*\)".*/\1/'`
+
+if ! svn copy $SVNROOT/trunk $SVNROOT/tags/v$VERSION -m "Tagged release $VERSION"
+then
+	echo failed to make a tag
+fi
+
+DIRNAME=cppcms-$VERSION
+mv current-release $DIRNAME
+cd $DIRNAME
+tar -xjf cppcms_boost.tar.bz2
+rm cppcms_boost.tar.bz2
+doxygen
+cd ..
+tar -cjf $DIRNAME.tar.bz2 $DIRNAME
+rm -fr $DIRNAME
+
+tar -xjf $DIRNAME.tar.bz2
+cd $DIRNAME
+mkdir build
+cd build
+if cmake -DCMAKE_BUILD_TYPE=Debug -DDISABLE_STATIC=ON .. && make && make test
+then
+	cd ..
+	rm -fr build
+	echo "Done ok"
+	DOCFILE=cppcms_ref_v`printf $VERSION | sed 's/\./_/g'`
+	cp -a doc/doxygen/html/  /home/artik/vserver-www/www/$DOCFILE
+	cd /home/artik/vserver-www/www
+	rm cppcms_ref_v0_99
+	ln -s $DOCFILE cppcms_ref_v0_99
+else
+	echo Failed!
+fi
+
+
+
+
+
