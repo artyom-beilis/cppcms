da68aa9e0e7ecb497001d6dbcee83ebf790ef874
Artyom Beilis <artyomtnk@yahoo.com>
2011-10-04 20:14:16 +0000

    Updated threading support fixes for win5 and win6

--- booster/lib/thread/src/thread_win6.cpp
+++ booster/lib/thread/src/thread_win6.cpp
@@ -27,130 +27,7 @@
 //
 
 namespace booster {
-
-	class win_thread_data : public refcounted {
-	public:
-		win_thread_data(function<void()> const &cb) :
-			handle_(0),
-			is_complete_(false),
-			callback_(cb)
-		{
-		}
-
-		void transfer_handle_ownership(HANDLE h)
-		{
-			{
-				unique_lock<mutex> guard(lock_);
-				if(!is_complete_) {
-					handle_ = h;
-					h = 0;
-				}
-			}
-
-			if(h) {
-				CloseHandle(h);
-			}
-		}
-		void run()
-		{
-			try {
-				callback_();
-				callback_ = function<void()>();
-			}
-			catch(...) {}
-			
-			unique_lock<mutex> guard(lock_);
-			is_complete_ = true;
-		}
-		~win_thread_data()
-		{
-			try {
-				unique_lock<mutex> guard(lock_);
-				if(handle_) {
-					CloseHandle(handle_);
-					handle_ = 0;
-				}
-			}
-			catch(...) {}
-		}
-	private:
-		mutex lock_;
-		HANDLE handle_;
-		bool is_complete_;
-		function<void()> callback_;
-	};
-	
-	struct thread::data {
-		intrusive_ptr<win_thread_data> shared;
-		HANDLE h;
-	};
-
-
-	extern "C" void *booster_thread_func(void *p)
-	{
-		// Do not add reference count as it was added upon sucesseful thread creation
-		intrusive_ptr<win_thread_data> d(reinterpret_cast<win_thread_data *>(p),false);
-		try {
-			d->run();
-		}
-		catch(...) {
-		}
-		_endthreadex(0);
-		
-		return 0;
-	}
-	unsigned WINAPI booster_real_thread_func(void *p) { booster_thread_func(p); return 0; }
-
-	thread::thread(function<void()> const &cb) :
-		d(new thread::data)
-	{
-		d->shared=new win_thread_data(cb);
-
-		uintptr_t p=_beginthreadex(0,0,booster_real_thread_func,d->shared.get(),0,0);
-		if(p!=0) {
-			// we want to transfer ownership to the thread explicitly
-			intrusive_ptr_add_ref(d->shared.get());
-		}
-		else {
-			throw system::system_error(system::error_code(errno,system::system_category));
-		}
-		
-		d->h=(HANDLE)(p);
-	}
-	void thread::detach()
-	{
-		if(d->h && d->shared) {
-			d->shared->transfer_handle_ownership(d->h);
-			d->h = 0;
-			d->shared = 0;
-		}
-	}
-	thread::~thread()
-	{
-		try {
-			detach();
-		}
-		catch(...){}
-	}
-
-	void thread::join()
-	{
-		if(d->h) {
-			WaitForSingleObject(d->h,INFINITE);
-			d->h = 0;
-			d->shared = 0;
-		}
-	}
-
-	unsigned thread::hardware_concurrency()
-	{
-		SYSTEM_INFO info=SYSTEM_INFO();
-		GetSystemInfo(&info);
-		return info.dwNumberOfProcessors;
-	}
-
 	struct mutex::data {
-		/// There is no - non - recursive mutex, so we can't do it without this
 		CRITICAL_SECTION m;
 	};
 
@@ -160,27 +37,17 @@ namespace booster {
 	}
 	mutex::~mutex()
 	{
-	}
-	void mutex::lock() { 
-		EnterCriticalSection(&d->m);
-	}
-	void mutex::unlock() { 
-		LeaveCriticalSection(&d->m);
-	}
-
-	struct recursive_mutex::data { CRITICAL_SECTION m; };
-
-	recursive_mutex::recursive_mutex() : d(new data)
-	{
-		InitializeCriticalSection(&d->m);
-	}
-	recursive_mutex::~recursive_mutex()
-	{
 		DeleteCriticalSection(&d->m);
 	}
-	void recursive_mutex::lock() { EnterCriticalSection(&d->m);}
-	void recursive_mutex::unlock() { LeaveCriticalSection(&d->m); }
-
+	void mutex::lock() 
+	{
+		EnterCriticalSection(&d->m);
+	}
+	void mutex::unlock() 
+	{ 
+		LeaveCriticalSection(&d->m);
+	}
+	
 	struct shared_mutex::data { SRWLOCK m; bool ex; };
 	shared_mutex::shared_mutex() : d(new data)
 	{
@@ -229,6 +96,10 @@ namespace booster {
 		SleepConditionVariableCS(&d->c,&(m.mutex()->d->m),INFINITE);
 	}
 
+	/*
+	 * This currently does not work...
+	 * Windows got Deadlock during destruction of the TLS key
+	 
 	namespace details {
 		extern "C" {
 			static WINAPI void booster_fls_key_destroyer(void *p)
@@ -271,6 +142,7 @@ namespace booster {
 		}
 	} // details
 
+	*/
 } // booster
 
 
