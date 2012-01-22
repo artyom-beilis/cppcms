5fe49906ae094af1ee6f7e194c89312fd3fae960
Artyom Beilis <artyomtnk@yahoo.com>
2012-01-22 22:20:43 +0000

    Updated Report to HTML script for better consistency

--- tools/report_to_html.py
+++ tools/report_to_html.py
@@ -4,23 +4,18 @@ import re
 import sys
 import datetime
 
-dic = { 
-	'arm':'<td>GCC-4.1</td><td>Linux 2.6</td><td>armel</td>', 
-	#'gcc_mac':'<td>GCC-4.2</td><td>Mac OS X (10.6)</td><td>x86_64</td>', 
-	#'gcc_43_32':'<td>GCC-4.3</td><td>Linux 2.6</td><td>x86</td>', 
-	'clangcc':'<td>Clang-2.9</td><td>Linux 2.6</td><td>x86_64</td>', 
-	'gcc_44':'<td>GCC-4.4</td><td>Linux 2.6</td><td>x86_64</td>', 
-	'gcc_44_stlport':'<td>GCC-4.4/STLPort 5.2</td><td>Linux 2.6</td><td>x86_64</td>', 
-	#'gcc_45':'<td>GCC-4.5</td><td>Linux 2.6</td><td>x86_64</td>', 
-	#'gcc_450x':'<td>GCC-4.5 (C++0x mode)</td><td>Linux 2.6</td><td>x86_64</td>', 
-	'intel':'<td>Intel 11.0</td><td>Linux 2.6</td><td>x86_64</td>', 
-	'bsd_gcc':'<td>GCC-4.2</td><td>FreeBSD 8.0</td><td>x86</td>', 
-	'solaris_gcc':'<td>GCC-3.4</td><td>OpenSolaris 2009.06</td><td>x86</td>',
-	'solaris_suncc':'<td>Sun Studio 5.11</td><td>OpenSolaris 2009.06</td><td>x86</td>',
-	'win_gcc':'<td>GCC-4.5/MinGW</td><td>Windows XP</td><td>x86</td>', 
-	'win_msvc':'<td>MSVC 2008</td><td>Windows XP</td><td>x86</td>', 
-	#'win_cyg':'<td>GCC-4.3</td><td>Windows XP/Cygwin</td><td>x86</td>', 
-}
+dic = [ \
+	[ 'gcc_44', '<td>Linux 2.6.32</td><td>GCC-4.4</td><td>x86_64</td>' ] , 
+	[ 'gcc_44_stlport', '<td>Linux 2.6.32</td><td>GCC-4.4/STLPort 5.2</td><td>x86_64</td>'],
+	[ 'clangcc', '<td>Linux 2.6.32</td><td>Clang-2.9</td><td>x86_64</td>'],
+	[ 'intel', '<td>Linux 2.6.32</td><td>Intel 12.1</td><td>x86_64</td>'], 
+	[ 'arm', '<td>Linux 2.6.32</td><td>GCC-4.1</td><td>armel</td>'],
+	[ 'bsd_gcc', '<td>FreeBSD 8.0</td><td>GCC-4.2</td><td>x86</td>'],
+	[ 'solaris_gcc', '<td>OpenSolaris 2009.06</td><td>GCC-3.4</td><td>x86</td>'],
+	[ 'solaris_suncc', '<td>OpenSolaris 2009.06</td><td>Sun Studio 5.11</td><td>x86</td>'],
+	[ 'win_gcc', '<td>Windows XP SP2</td><td>GCC-4.5</td><td>x86</td>'],
+	[ 'win_msvc', '<td>Windows XP SP2</td><td>MSVC 2008</td><td>x86</td>'],
+]
 
 r=r'(\w+)\s+-\s*(pass|fail)';
 
@@ -37,8 +32,8 @@ print """
 print datetime.datetime.now().strftime('<h2>Tested at: %Y-%m-%d %H:%M</h2>')
 
 print """
-<table cellpadding="3" cellspacing="5" >
-<tr><th>Compiler</th><th>Operating System</th><th>Platform</th><th>Status</th></tr>
+<table cellpadding="3" cellspacing="0" border="1" >
+<tr><th width="20%" >Operating System</th><th width="20%" >Compiler</th><th width="20%">Platform</th><th width="20%">Status</th></tr>
 """
 
 all={}
@@ -48,7 +43,7 @@ for line in sys.stdin:
 	if m:
 		all[m.group(1)] = m.group(2)
 
-for key,value in dic.items():
+for key,value in dic:
 	status = 'not tested'
 	if key in all:
 		status = all[key]
