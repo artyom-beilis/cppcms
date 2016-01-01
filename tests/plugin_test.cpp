586580b3cc2d8b0d4d09c3e550b0271ec65573d8
Artyom Beilis <artyomtnk@yahoo.com>
2016-01-01 21:35:13 +0000

    Improved plugin test

--- tests/plugin_test.cpp
+++ tests/plugin_test.cpp
@@ -4,15 +4,22 @@
 #include "test.h"
 
 
-int main()
+int main(int argc,char **argv)
 {
+	if(argc!=2) {
+		std::cerr << "Usage plugin_test /path/to/the/plugin/dir" << std::endl;
+		return 1;
+	}
+	std::string path = argv[1];
 	try {
-		booster::shared_object obj(booster::shared_object::name("plugin"));
+		booster::shared_object obj(path + "/" + booster::shared_object::name("plugin"));
 		{
+			std::cout << "- Normal call" << std::endl;
 			booster::callback<std::string(std::string const &)> cb;
 			cb = cppcms::plugin::manager::entry<std::string(std::string const &)>("foo::lower");
 			TEST(cb("Hello World")=="hello world");
 			try {
+				std::cout << "- Bad signature" << std::endl;
 				cppcms::plugin::manager::entry<std::string(std::string &)>("foo::lower");
 				TEST(!"Never Get There");
 			}
@@ -20,9 +27,10 @@ int main()
 			catch(...) { throw std::runtime_error("Something else thrown"); }
 		} 
 		obj.close();
+		std::cout << "- Unload" << std::endl;
 		try {
 			cppcms::plugin::manager::entry<std::string(std::string const &)>("foo::lower");
-			std::cerr << "Must Not get there!" <<std::endl;
+			std::cerr << "Must Not get there:" << __LINE__<<std::endl;
 			return 1;
 		}
 		catch(cppcms::cppcms_error const &) {}
