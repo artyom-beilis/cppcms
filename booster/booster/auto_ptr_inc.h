d8c7b9a33f510ddd19bd2b3a15b92854531b3919
Artyom Beilis <artyomtnk@yahoo.com>
2017-03-06 13:36:31 +0200

    Fix for older gcc versions

--- booster/booster/auto_ptr_inc.h
+++ booster/booster/auto_ptr_inc.h
@@ -8,14 +8,17 @@
 #ifndef BOOSTER_AUTO_PTR_INC_H
 #define BOOSTER_AUTO_PTR_INC_H
 
-#include <functional>
-#if defined __GLIBCXX__
-# include <bits/c++config.h>
-# ifdef _GLIBCXX_DEPRECATED
-#   undef _GLIBCXX_DEPRECATED
-#   define _GLIBCXX_DEPRECATED
-# endif
+#if __cplusplus >= 201103L
+#  include <functional>
+#  if defined __GLIBCXX__
+#   include <bits/c++config.h>
+#   ifdef _GLIBCXX_DEPRECATED
+#     undef _GLIBCXX_DEPRECATED
+#     define _GLIBCXX_DEPRECATED
+#   endif
+#  endif
 #endif
+
 #include <memory>
 
 #endif // BOOSTER_AUTO_PTR_INC_H
