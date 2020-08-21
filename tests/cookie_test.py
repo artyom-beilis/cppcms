f8163c6765aa6dd03f42a73d809d30d25a1aa2c4
Artyom Beilis <artyomtnk@yahoo.com>
2020-08-22 00:43:30 +0300

    Merges from cpp11

--- tests/cookie_test.py
+++ tests/cookie_test.py
@@ -2,25 +2,30 @@
 # coding=utf-8
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 
-import httplib
-import sys
+from __future__ import print_function
 
+try:
+    import httplib
+except:
+    import http.client as httplib
+import sys
+import traceback
 
 def check(name,A,B):
     if A != B:
-        print "Error :" + name 
-        print "-----Actual--"
-        print A,"---Expected--"
-        print B,"-------------"
+        print("-----Actual--")
+        print(A,"---Expected--")
+        print(B,"-------------")
         f1=open('actual.txt','wb')
         f2=open('expected.txt','wb')
         f1.write(A)
         f2.write(B)
         f1.close()
         f2.close()
+        traceback.print_tb()
         sys.exit(1)
     else:
-        print "Ok:"+name
+        print("Ok:",name)
 
 class Nothing:
     pass
@@ -31,12 +36,12 @@ def test_valid(headers,expected):
     r=h.getresponse()
     check(headers,r.read(),expected)
 
-test_valid('test=foo','test:foo\n')
-test_valid('test=','test:\n')
-test_valid('test','test:\n')
-test_valid('test="\xD7\xA9\xD7\x9C\xD7\x95\xD7\x9D"; z=x','test:\xD7\xA9\xD7\x9C\xD7\x95\xD7\x9D\nz:x\n');
-test_valid('test="\xD7\xA9\\"\xD7\x9C\xD7\x95\xD7\x9D"; z=x','test:\xD7\xA9"\xD7\x9C\xD7\x95\xD7\x9D\nz:x\n');
-test_valid('a=\xD7\xA9\xD7\x9C\xD7\x95\xD7\x9D; z=x','z:x\n');
+test_valid(b'test=foo',b'test:foo\n')
+test_valid(b'test=',b'test:\n')
+test_valid(b'test',b'test:\n')
+test_valid(b'test="\xD7\xA9\xD7\x9C\xD7\x95\xD7\x9D"; z=x',b'test:\xD7\xA9\xD7\x9C\xD7\x95\xD7\x9D\nz:x\n');
+test_valid(b'test="\xD7\xA9\\"\xD7\x9C\xD7\x95\xD7\x9D"; z=x',b'test:\xD7\xA9"\xD7\x9C\xD7\x95\xD7\x9D\nz:x\n');
+test_valid(b'a=\xD7\xA9\xD7\x9C\xD7\x95\xD7\x9D; z=x',b'z:x\n');
 
 
 
